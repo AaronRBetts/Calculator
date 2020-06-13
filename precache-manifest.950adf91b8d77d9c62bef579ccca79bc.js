f0d153ccea23c6914f0f0220a61119dd58a40d69
AaronRBetts <aaronrbetts@gmail.com>
2020-06-13 12:50:45 +0300

    Updates

---
+++ precache-manifest.950adf91b8d77d9c62bef579ccca79bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427366c3c29979feac156532dbe938b3",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "93aae0e63dee653b9521",
+    "url": "/Calculator/static/css/main.d6cd3575.chunk.css"
+  },
+  {
+    "revision": "1a67cadf6565fc027496",
+    "url": "/Calculator/static/js/2.36b0bddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.36b0bddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93aae0e63dee653b9521",
+    "url": "/Calculator/static/js/main.55f9e2f9.chunk.js"
+  },
+  {
+    "revision": "a0843c05571dcd8658af",
+    "url": "/Calculator/static/js/runtime-main.939bd12a.js"
+  }
+]);
